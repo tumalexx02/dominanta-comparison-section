f0911634c238a82c162d4f5127f6471097060009
Aleksandr Tumanov <tumalexx02@gmail.com>
2024-06-19 18:20:05 +0500

    Added 5+ points support

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const comparisonBtns = document.querySelectorAll('.comparison__more-btn');
 const tableContainer = document.querySelector('.comparison__table-container');
 const shade = document.querySelector('.comparison__table-shade');
+const theadElementsCount = document.querySelectorAll('.comparison__table thead td').length;
+
+if (theadElementsCount > 4) {
+  tableContainer.style.overflowX = 'scroll';
+} else {
+  tableContainer.style.overflowX = 'auto';
+}
 
 comparisonBtns.forEach(comparisonBtn => comparisonBtn.addEventListener('click', (e) => {
   const comparisonContent = e.target.parentNode.parentNode.parentNode.querySelector('.comparison__table-content');
   const localComparisonBtn = e.target;
 
+  // console.log(theadElementsCount)
   localComparisonBtn.classList.toggle('comparison__more-btn_active');
   comparisonContent.classList.toggle('comparison__table-content_hidden');
 }))
@@ -19,3 +27,4 @@ tableContainer.addEventListener('scroll', () => {
     shade.classList.remove('comparison__table-shade_hidden');
   }
 })
+
